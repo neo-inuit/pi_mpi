f7ba705738d9cb2a25f8ca922c4f8655c4263107
Remi Michelas <remi.michelas@inria.fr>
2013-11-06 17:50:18 +0100

    added oar support not working yet

--- MPI_script.rb
+++ MPI_script.rb
@@ -3,42 +3,62 @@
 # idea
 #Put the LD LIBRARY in each shell env use the submission script for nef
 #script dedicated for each server or keep everything on kali side?
+#added opts gem to manage arguments
+#added job_delayed gem to manage long background task
 
 require 'net/ssh'
 
 # variables
-error=0
-stram=""
-hostname=""
-cmd=""
-login="rmichela"
-output="hello_mpi_out.txt"
-$cores=1
+$error=0
+$stream=""
+$hostname=""
+$cmd=""
+$g5k_geo="sophia"
+$login="rmichela"
+$output="pi_mpi_out.txt"
+$nodes=1
+$pbs_file="pi_mpi.pbs"
+$prog_name="pi_mpi"
+$arg=10000000
 $jobid=0
 
 #definitions
 scheduler_command = [[],[]]
-#scheduler_command = [["oarrun", "oarstat", "oarkill"], ["export LD_LIBRARY_PATH=/opt/openmpi/current/lib64 && mpirun.openmpi -np %{cores} ./hello_mpi %{arg}", "qstat | grep #{login}", "qkill"]]
+# dirty $cmd for nef no jobid return
+#["export LD_LIBRARY_PATH=/opt/openmpi/current/lib64 && mpirun.openmpi -np %{nodes} ./hello_mpi %{arg}"]
 
-scheduler_command = [["oarrun", "oarstat", "oarkill"], ["qsub -l \"nodes=%{cores}\"", "qstat | grep #{login}", "qkill"]]
+scheduler_command = [["mpirun.openmpi -mca btl ^openib -np %{nodes} --stdout %{output} %{prog} %{arg}", "oarstat %{jobid}", "oardel %{jobid}"], ["qsub -l nodes=%{nodes} -o %{output} %{pbs}", "qstat %{jobid}", "qdel %{jobid}"]]
 
 #fonctions
 def help
   puts "This script need 2 or 3 arguments"
   puts "First argument which scheduler : o for OAR and t for TORQUE"
   puts "Second argument what you want to do : run or stat or kill"
-  puts "Third argument only if you choose run, the number of cores"
+  puts "Third argument only if you choose run, the number of nodes"
 end
 
 def launch(hostname,login,cmd)
   begin
+    puts("ssh connection #{hostname} #{login}")
     ssh = Net::SSH.start(hostname,login)
+    
+    # if oar choose
+    if (!scheduler)
+      puts("oar choose")
+      ssh2 = Net::SSH.start($g5k_geo,login)
+      #node reservation
+      oar = ssh.exec!("oarsub -l /nodes=%{nodes}" % {nodes: $nodes})
+    end
+    puts("execution cmd #{cmd}")
     res = ssh.exec!(cmd)
     # read the output file
     res2 = ssh.exec!("cat #{output}")
+    if (!scheduler)
+      ssh2.close
+    end
     ssh.close
     puts res
-    jobid = res
+    $jobid = res
     puts res2
   rescue
     puts "Unable to connect to #{hostname}"
@@ -57,8 +77,8 @@ else
     then puts "you choose TORQUE scheduler"
   scheduler=1
   elsif ( ! scheduler )
-    error=1
-    stream = "no good scheduler option"
+    $error=1
+    $stream = "no good scheduler option"
   end
   if ARGV[1].eql?("run")
     then puts "you choose run operation"
@@ -67,8 +87,8 @@ else
       then help
       exit
     else
-      $cores=ARGV[2]
-      puts "you choose #{$cores} cores"      
+      $nodes=ARGV[2]
+      puts "you choose #{$nodes} nodes"      
     end    
   elsif ARGV[1].eql?("stat")
     then puts "you choose stat operation"
@@ -77,23 +97,23 @@ else
     then puts "you choose kill operation"
   state=2
   elsif ( ! state )
-    error=2
-    stream = "no good state option"
+    $error=2
+    $stream = "no good state option"
   end
 
-  if (error>0) then puts stream
+  if ($error>0) then puts stream
   elsif
 
   if (scheduler==0)
-  then hostname = "g5k"
+  then $hostname = "g5k"
   elsif (scheduler==1)
-  then hostname = "nef-devel"
+  then $hostname = "nef-devel"
   end
-    puts hostname
-    # added cores number if needed (for run cmd)
-    cmd = scheduler_command[scheduler][state] % {cores: $cores, arg: "world"}
-    # cmd displayed
-    puts cmd
-    launch(hostname,login,cmd)
+    puts $hostname
+    # added nodes number if needed (for run $cmd)
+    $cmd = scheduler_command[scheduler][state] % {nodes: $nodes, output: $output, pbs: $pbs_file, jobid: $jobid, prog: $prog_name, arg: $arg}
+    # $cmd displayed
+    puts $cmd
+    launch($hostname,$login,$cmd)
   end
 end
